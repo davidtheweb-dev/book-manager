dada81526efb3d952379ab16ac00fe4cf0de3480
davidheweb <davidhermosilladuro@gmail.com>
2023-05-10 15:11:15 +0200

    Added uuid library and createBook() in BookForm

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,70 +1,154 @@
 <script>
+import { v4 as uuidv4 } from 'uuid';
+
 export default {
   name: 'BookForm',
+  data() {
+    return {
+      title: '',
+      author: '',
+      year: '',
+      genreInput: '',
+      description: '',
+      pages: '',
+      editorial: '',
+      language: '',
+      isbn: '',
+      price: '',
+      stock: '',
+      favourite: '',
+      rating: '',
+      image: '',
+      error: false,
+    };
+  },
+  methods: {
+    createBook() {
+      if (!this.title || !this.author || !this.description || !this.year) {
+        this.error = true;
+        return;
+      }
+
+      const genres = this.genreInput.split(',').map((genre) => genre.trim());
+
+      const book = {
+        id: uuidv4(),
+        title: this.title,
+        author: this.author,
+        year: parseInt(this.year),
+        pages: parseInt(this.pages),
+        cover: this.image,
+        genre: genres,
+        description: this.description,
+        editorial: this.editorial,
+        language: this.language,
+        isbn: this.isbn,
+        price: this.price,
+        stock: this.stock,
+        favourite: this.favourite,
+        rating: this.rating,
+      };
+
+      this.$emit('add-book', book);
+
+      this.title = '';
+      this.author = '';
+      this.year = '';
+      this.pages = '';
+      this.cover = '';
+      this.genreInput = '';
+      this.description = '';
+      this.editorial = '';
+      this.language = '';
+      this.isbn = '';
+      this.price = '';
+      this.stock = '';
+      this.favourite = '';
+      this.rating = '';
+      this.error = false;
+    },
+  },
 };
 </script>
 
 <template>
   <div class="book-form">
-    <form class="book-form__form">
+    <form class="book-form__form" @submit.prevent="createBook">
       <div class="book-form__form-group">
         <label for="title">Title</label>
-        <input id="title" type="text" required />
+        <input id="title" type="text" v-model="title" />
       </div>
       <div class="book-form__form-group">
         <label for="author">Author</label>
-        <input id="author" type="text" required />
+        <input id="author" type="text" v-model="author" />
       </div>
       <div class="book-form__form-group">
         <label for="description">Description</label>
-        <textarea id="description" required></textarea>
+        <textarea id="description" v-model="description"></textarea>
       </div>
       <div class="book-form__form-group">
         <label for="year">Year</label>
-        <input id="year" type="number" required />
+        <input id="year" type="number" v-model="year" />
       </div>
       <div class="book-form__form-group">
         <label for="pages">Pages</label>
-        <input id="pages" type="number" required />
+        <input id="pages" type="number" v-model="pages" />
       </div>
       <div class="book-form__form-group">
         <label for="genre">Genre</label>
-        <input id="genre" type="text" required />
+        <input id="genre" type="text" v-model="genreInput" />
       </div>
       <div class="book-form__form-group">
         <label for="editorial">Editorial</label>
-        <input id="editorial" type="text" required />
+        <input id="editorial" type="text" v-model="editorial" />
       </div>
       <div class="book-form__form-group">
         <label for="language">Language</label>
-        <input id="language" type="text" required />
+        <select id="language" v-model="language">
+          <option value="">Select a language</option>
+          <option value="Spanish">Spanish</option>
+          <option value="English">English</option>
+          <option value="French">French</option>
+          <option value="Italian">Italian</option>
+          <option value="German">German</option>
+        </select>
       </div>
       <div class="book-form__form-group">
         <label for="isbn">ISBN</label>
-        <input id="isbn" type="text" required />
+        <input id="isbn" type="text" v-model="isbn" />
       </div>
       <div class="book-form__form-group">
         <label for="price">Price</label>
-        <input id="price" type="number" required />
+        <input id="price" type="number" v-model="price" />
       </div>
       <div class="book-form__form-group">
         <label for="stock">Stock</label>
-        <input id="stock" type="number" required />
+        <input id="stock" type="number" v-model="stock" />
       </div>
       <div class="book-form__form-group">
         <label for="favourite">Favourite</label>
-        <input id="favourite" type="checkbox" />
+        <input id="favourite" type="checkbox" v-model="favourite" />
       </div>
       <div class="book-form__form-group">
         <label for="rating">Rating</label>
-        <input id="rating" type="number" min="0" max="5" step="1" required />
+        <select id="rating" v-model="rating">
+          <option value="">Select a rating</option>
+          <option value="1">1 star</option>
+          <option value="2">2 stars</option>
+          <option value="3">3 stars</option>
+          <option value="4">4 stars</option>
+          <option value="5">5 stars</option>
+        </select>
       </div>
       <div class="book-form__form-group">
         <label for="cover">Image</label>
-        <input id="cover" type="url" required />
+        <input id="cover" type="url" v-model="image" />
       </div>
       <div class="book-form__form-group">
         <button class="book-form__submit" type="submit">Add book</button>
+      </div>
+      <div v-if="error" class="book-form__error">
+        Error submitting form. Please try again.
       </div>
     </form>
   </div>
@@ -88,7 +172,8 @@ export default {
   font-size: 14px;
 }
 .book-form__form-group input,
-.book-form__form-group textarea {
+.book-form__form-group textarea,
+.book-form__form-group select {
   width: 80%;
   padding: 10px;
   border: 1px solid #e5e5e5;
@@ -97,7 +182,8 @@ export default {
   text-align: left;
 }
 .book-form__form-group input:focus,
-.book-form__form-group textarea:focus {
+.book-form__form-group textarea:focus,
+.book-form__form-group select:focus {
   border-color: #000;
 }
 .book-form__form-group textarea {
@@ -115,5 +201,10 @@ export default {
 }
 .book-form__submit:hover {
   background-color: #43a047;
+}
+.book-form__error {
+  color: red;
+  font-size: 14px;
+  margin-top: 10px;
 }
 </style>
